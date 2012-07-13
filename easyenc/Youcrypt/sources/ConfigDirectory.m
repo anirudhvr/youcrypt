fbb99a9f14caf47af2df0f4d56fe3571eb840dcb
Hardik Ruparel <hardik988@gmail.com>
2012-07-13 16:55:48 -0700

    Moved mixpanel to AppDelegate and now generating UUID and saving as ~/.youcrypt/uuid.txt

--- easyenc/Youcrypt/sources/ConfigDirectory.m
+++ easyenc/Youcrypt/sources/ConfigDirectory.m
@@ -8,7 +8,7 @@
 
 #import "ConfigDirectory.h"
 #import "libFunctions.h"
-#import "logging.h"
+#import "Contrib/Lumberjack/logging.h"
 
 @implementation ConfigDirectory
 
@@ -18,6 +18,7 @@
 @synthesize youCryptLockFile;
 @synthesize youCryptListFile;
 @synthesize firstRun;
+@synthesize youcryptUserUUID;
 
 -(id)init
 {
@@ -29,6 +30,7 @@
     youCryptTmpDir = [homedir stringByAppendingPathComponent:@"/.youcrypt/tmp"];
     youCryptLogDir = [homedir stringByAppendingFormat:@"/.youcrypt/logs"];
     youCryptListFile = [homedir stringByAppendingPathComponent:@"/.youcrypt/dirs.plist"];
+    youcryptUserUUID = [homedir stringByAppendingPathComponent:@"/.youcrypt/uuid.txt"];
 
     if (![[NSFileManager defaultManager] fileExistsAtPath:[homedir stringByAppendingPathComponent:@"/.youcrypt"]]) {
         firstRun = YES;
@@ -39,6 +41,13 @@
         firstRun = NO;
     }
     
+    NSString *uuid = [[NSProcessInfo processInfo] globallyUniqueString];
+    NSError *error;
+    [uuid writeToFile:youcryptUserUUID atomically:YES encoding:NSASCIIStringEncoding error:&error];
+    if(error) {
+        DDLogVerbose(@"Could not create uuid.txt : %@",[error localizedDescription]);
+    }
+    DDLogVerbose(@"Client has been assigned UUID: %@",uuid);
     return self;
 }
 
